be5f48e5ab709710428cfe3c4a1aac4366802ce5
Asma-Abed <abed.asma@hotmail.com>
2022-08-22 14:39:40 +0100

    dropdown nav added

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const nav = document.getElementById('nav');
 const navLogo = document.getElementById('nav-logo');
 const navLink = document.querySelectorAll('.nav__link');
 const navSearchIcon = document.querySelector('.nav__search-icon');
+const navDropIcon = document.querySelector('.nav__dropdown-icon');
 const btnViolet = document.getElementById('btn-violet');
 const btnWhite = document.getElementById('btn-white');
 
@@ -19,21 +20,23 @@ const stickyNav = (entries) => {
     navLogo.src = '../img/logo-black.png';
     navLogo.className = 'header__logo-sticky';
     navLink.forEach((el) => {
-      el.className = 'nav__link nav__link--black';
+      el.classList.add('nav__link--black');
     });
     navSearchIcon.classList.add('nav__search-icon--black');
     btnViolet.classList.add('btn--border-violet');
     btnWhite.classList.add('btn--violet');
+    navDropIcon.classList.add('nav__dropdown-icon--black');
   } else {
     nav.classList.remove('nav__sticky');
     navLogo.src = '../img/logo.png';
     navLogo.className = 'header__logo';
     navLink.forEach((el) => {
-      el.className = 'nav__link';
+      el.classList.remove('nav__link--black');
     });
     navSearchIcon.classList.remove('nav__search-icon--black');
     btnViolet.classList.remove('btn--border-violet');
     btnWhite.classList.remove('btn--violet');
+    navDropIcon.classList.remove('nav__dropdown-icon--black');
   }
 };
 
